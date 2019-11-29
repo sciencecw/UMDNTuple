c373a98c96df605ebdad4017a6fd995c0e53293a
Markus Seidel <markus.seidel@cern.ch>
2019-11-29 09:46:41 -0500

    Fix typoe for prefiringweight

--- UMDNTuple/python/__init__.py
+++ UMDNTuple/python/__init__.py
@@ -1,3 +1,3 @@
 #Automatically created by SCRAM
 import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/UMDNTuple/UMDNTuple/',1)[0])+'/cfipython/slc6_amd64_gcc630/UMDNTuple/UMDNTuple')
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/UMDNTuple/UMDNTuple/',1)[0])+'/cfipython/slc6_amd64_gcc700/UMDNTuple/UMDNTuple')
